7f06b2bb527f52d63bda78dc1a3c9234b1a8f05b
Anil Patnaik <tanujapatnaik@Tanujas-MacBook-Air.local>
2020-02-27 19:17:38 +1300

    Updates

---
+++ precache-manifest.b702490efa57b384130ce4d20477d346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6335147eedd32e0507f5378f9e4a24f",
+    "url": "/number-game/index.html"
+  },
+  {
+    "revision": "3d59218f21ad66b0ec0c",
+    "url": "/number-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f27d42b74cb77d3e34f6",
+    "url": "/number-game/static/js/2.3c4da5de.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/number-game/static/js/2.3c4da5de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d59218f21ad66b0ec0c",
+    "url": "/number-game/static/js/main.7dc09fe2.chunk.js"
+  },
+  {
+    "revision": "676c12f8058a8a0c45db",
+    "url": "/number-game/static/js/runtime-main.3e5c2899.js"
+  }
+]);
